5098ee0b48d18298ea4953f154158787fbba5ccd
William Öqvist <williamoqvist@hotmail.com>
2023-03-15 19:22:37 +0100

    setAlarm wip

--- index.d.ts
+++ index.d.ts
@@ -73,7 +73,7 @@ declare namespace SendIntentAndroid {
   const requestIgnoreBatteryOptimizations: () => Promise<boolean>
   const showIgnoreBatteryOptimizationsSettings: () => void
   const openAppWithUri: (intentUri: string, extras?: { [index: string]: string }) => Promise<boolean>
-  const setTimer: (seconds: number, message: string, skip_ui: boolean)=> void
+  const setTimer: (seconds: string, message: string, skip_ui: boolean)=> void
   const TEXT_PLAIN: unique symbol
   const TEXT_HTML: unique symbol
 }
